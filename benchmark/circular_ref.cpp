74c77ad696597a1256fc7ecbc795b42da2c53187
게임공학과 학생 박재성 <2023180014@tukorea.ac.kr>
2025-06-12 22:30:29 +0900

    circuar_ref update

--- benchmark/circular_ref.cpp
+++ benchmark/circular_ref.cpp
@@ -33,20 +33,44 @@ void test() {
 	use_ptr<B> b(new B(2));
 	use_ptr<C> c(new C(3));
 
-	use_guard<A> guard(a);
+	use_guard<A> guard1(a);
 	use_guard<B> guard2(b);
 	use_guard<C> guard3(c);
 
-	guard->ptr.store(b);     // A → B
+	guard1->ptr.store(b);     // A → B
 	guard2->ptr.store(c);     // B → C
 	guard3->ptr.store(a);     // C → A (업캐스팅 OK)
+	{
+		use_guard<B> guard1_1(guard1->ptr);
+		use_guard<C> guard2_1(guard2->ptr);
+		use_guard<A> guard3_1(guard3->ptr);
+		{
+			use_guard<C> guard1_2(guard1_1->ptr);
+			use_guard<A> guard2_2(guard2_1->ptr);
+			use_guard<B> guard3_2(guard3_1->ptr);
+			{
+				use_guard<A> guard1_3(guard1_2->ptr);
+				use_guard<B> guard2_3(guard2_2->ptr);
+				use_guard<C> guard3_3(guard3_2->ptr);
 
-	// 다형성도 가능
-	guard->say();           // "C"
+				guard1_3->say();
+				guard2_3->say();
+				guard3_3->say();
+			}
+
+			guard1_2->say();
+			guard2_2->say();
+			guard3_2->say();
+		}
+
+		guard1_1->say();
+		guard2_1->say();
+		guard3_1->say();
+	}
+
+	guard1->say();  
 	guard2->say();
-	guard3->say(); // "C" (A → B → C)
-
-	// 해제
+	guard3->say(); 
 }
 int main() {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
